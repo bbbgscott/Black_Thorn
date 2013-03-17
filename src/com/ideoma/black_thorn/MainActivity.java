99283b74cf40890247db93177352d23bcb5497b4
Brian Bump <bbump@csumb.edu>
2013-03-17 13:02:09 -0700

    efawef

--- src/com/ideoma/black_thorn/MainActivity.java
+++ src/com/ideoma/black_thorn/MainActivity.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Random;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -26,6 +27,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
@@ -34,14 +36,21 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnMyLocationChangeListener{
 
 	//LatLng[] montereyArrayCoords = new LatLng[]{new LatLng(36.654360,-121.800420),new LatLng(36.654244,-121.799272),
 	//		new LatLng(36.654600,-121.799846),new LatLng(36.654360,-121.800420)};
 	LatLng[] montereyArrayCoords;
+	String[] facts;
+	int[] audioArray;
 	double latRadiusStart = 0.0005;
 	MediaPlayer myPlayer;
+	//Get Textview
+	TextView factsView;
+
 	
 	public final static String baseResPath = "android.resource://com.ideoma.black_thorn/";
 	
@@ -49,9 +58,20 @@ public class MainActivity extends Activity implements OnMyLocationChangeListener
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);		
+		facts = GetFactsFromResource(R.raw.facts);
+		factsView = (TextView)findViewById(R.id.MarqueeText);
+		MarqueeChangeTask mtask = new MarqueeChangeTask();
+		mtask.execute(facts);
 		
 		myPlayer = new MediaPlayer();
+		myPlayer = MediaPlayer.create(this, R.raw.convo1);
 		myPlayer.setLooping(false);
+		myPlayer.setOnCompletionListener(new OnCompletionListener(){
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				currentAudioPlaying = -1;
+			}
+		});
 		
 		LatLng monterey = new LatLng(36.654244, -121.799272);
 		
@@ -83,8 +103,11 @@ public class MainActivity extends Activity implements OnMyLocationChangeListener
 	     	.width(5)
 	     	.color(Color.RED)
 	     );
+		
 	}
-
+	
+	int currentAudioPlaying = -1;
+	
 	@Override
 	public void onMyLocationChange(Location loc) {
 		Log.i("tittysprinkles","myLocationChanged! " + loc.getLatitude() + " " + loc.getLongitude());
@@ -93,16 +116,66 @@ public class MainActivity extends Activity implements OnMyLocationChangeListener
 			LatLng ll = montereyArrayCoords[i];
 			double dx = Math.abs(loc.getLatitude() - ll.latitude);
 			double dy = Math.abs(loc.getLongitude() - ll.longitude);
-			double d = Math.sqrt(dx*dx - dy*dy);
+			double d = Math.sqrt(dx*dx + dy*dy);
+			Log.i("tittysprinkles","D: " + d);
 			if(d < latRadiusStart)
 			{
-				Log.i("tittysprinkles","Radius activated! " + d);
+				Log.i("tittysprinkles","Radius activated! " + d + "  " + i);
 				try {
-					myPlayer.reset();
-					myPlayer.setDataSource(baseResPath + R.raw.convo1);
+					switch(i)
+					{
+					case(0):
+						Log.i("tittysprinkles","Case 0 ");
+						if(currentAudioPlaying != 0)
+						{
+							myPlayer.stop();
+							myPlayer.release();
+							myPlayer =null;
+							myPlayer = MediaPlayer.create(this, R.raw.convo1);
+							currentAudioPlaying = 0;
+							myPlayer.start();
+						}
+						break;
+					case(1):
+						Log.i("tittysprinkles","Case 1 ");
+						if(currentAudioPlaying != 1)
+						{
+							myPlayer.stop();
+							myPlayer.release();
+							myPlayer =null;
+							myPlayer = MediaPlayer.create(this, R.raw.convo2);
+							currentAudioPlaying = 1;
+							myPlayer.start();
+						}
+						break;
+					case(2):
+						Log.i("tittysprinkles","Case 2 ");
+						if(currentAudioPlaying != 2)
+						{
+							myPlayer.stop();
+							myPlayer.release();
+							myPlayer =null;
+							myPlayer = MediaPlayer.create(this, R.raw.convo3);
+							currentAudioPlaying = 2;
+							myPlayer.start();
+						}
+						break;
+					case(3):
+						Log.i("tittysprinkles","Case 3 ");
+						if(currentAudioPlaying != 3)
+						{
+							myPlayer.stop();
+							myPlayer.release();
+							myPlayer =null;
+							myPlayer = MediaPlayer.create(this, R.raw.convo4);
+							currentAudioPlaying = 3;
+							myPlayer.start();
+						}
+						break;
+					}
+					//myPlayer.setDataSource(baseResPath + R.raw.convo1);
 					Log.i("tittysprinkles","Data source set: " + baseResPath + R.raw.convo1);
-					myPlayer.prepare();
-					myPlayer.start();
+					//myPlayer.prepare();
 				} catch (Exception e) {
 					Log.e("tittysprinkles","Error setting player's data source! " + e.getMessage());
 				}
@@ -117,16 +190,27 @@ public class MainActivity extends Activity implements OnMyLocationChangeListener
 		return true;
 	}
 
+	
 	public class MarqueeChangeTask extends AsyncTask {
 		long startTime = 0;
-		long nextTime = 2*(60*1000);
+		long nextTime = 3000;
 		Calendar c;
 		@Override
 		protected Object doInBackground(Object... arg0) {
-			if(c.getTimeInMillis()-startTime >= nextTime)
+			boolean b = true;
+			String[] farray = GetFactsFromResource(R.raw.facts);
+			Log.e("tittysprinkles","farray length: " + farray.length);
+			while(b)
 			{
-				RestartTime(c);
-				//Change text
+				int i = 0; 
+				Random rand = new Random();
+				i = rand.nextInt(farray.length-1);
+				factsView.setText(farray[i]);
+				try {
+					Thread.sleep(nextTime);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 			return null;
 		}
@@ -165,6 +249,39 @@ public class MainActivity extends Activity implements OnMyLocationChangeListener
 			}
 			LatLng[] latlngarray = new LatLng[coords.size()];
 			coords.toArray(latlngarray);
+			return latlngarray;
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	String[] GetFactsFromResource(int res)
+	{
+		try {
+			ArrayList<String> factArray = new ArrayList<String>();
+			InputStream in = getResources().openRawResource(res);
+			XMLParser parser = new XMLParser();
+		
+			Log.e("titty_sprinkles","In: " + in.available());
+			
+			Document doc = parser.ParseXMLToDoc(in);
+			NodeList nList = parser.ParseDocByTagName(doc, "fact");
+			Log.e("tittysprinkles","bList length: " + nList.getLength());
+			for(int i = 0; i < nList.getLength(); i++)
+			{
+				String fact = "";
+				Element ele = (Element) nList.item(i);
+				fact = parser.GetTextValueByTagName(ele, "fact");
+				
+				Log.e("titty_sprinkles","fact: " + fact);
+				factArray.add(fact);
+			}
+			String[] latlngarray = new String[factArray.size()];
+			factArray.toArray(latlngarray);
 			return latlngarray;
 		} catch (SAXException e) {
 			e.printStackTrace();
--- src/com/ideoma/black_thorn/StartPageActivity.java
+++ src/com/ideoma/black_thorn/StartPageActivity.java
@@ -6,7 +6,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,7 +20,8 @@ public class StartPageActivity extends Activity
 {
 	
 	private final int OPEN_SECURITY_SETTINGS = 27;
-	
+	int currentAudioPlaying = -1;
+	MediaPlayer myPlayer;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -63,6 +68,42 @@ public class StartPageActivity extends Activity
 				startActivity(intent);
 			}
 		});
+		
+		final Context c = this;
+		myPlayer = new MediaPlayer();
+		
+		myPlayer = MediaPlayer.create(this, R.raw.convo1);
+		myPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		myPlayer.setLooping(false);
+		myPlayer.setOnCompletionListener(new OnCompletionListener(){
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				currentAudioPlaying = -1;
+			}
+		});
+		
+		 /*
+		new Thread(new Runnable(){
+			int i = 0;
+			public void run()
+			{
+				while(true)
+				{
+					Log.i("tittysprinkles","i: "+i);
+					
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					i++;
+					if(i>3)
+					{
+						i=0;
+					}
+				}
+			}
+		}).start();*/
 	}
 	
 	public void EnableGPS()
