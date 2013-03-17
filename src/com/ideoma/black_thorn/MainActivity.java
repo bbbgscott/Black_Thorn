9fccbb4df0edc031cadf103f55db3d8d8bdbf711
Brandon Layton <brandon0layton@yahoo.com>
2013-03-17 01:30:16 -0700

    LatLng get and mark

--- src/com/ideoma/black_thorn/MainActivity.java
+++ src/com/ideoma/black_thorn/MainActivity.java
@@ -1,5 +1,17 @@
 package com.ideoma.black_thorn;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -9,6 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -24,6 +37,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);		
 		
 		LatLng monterey = new LatLng(36.654244, -121.799272);
+		LatLng[] montereyArrayCoords;
 		
 		GoogleMap map;
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
@@ -37,6 +51,12 @@ public class MainActivity extends Activity {
 		LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		LocationListener mlocListener = new MyLocationListener();
 		mlocManager.*/
+		
+		montereyArrayCoords = GetGpsCoordsFromResource(R.xml.csumb_gps_coordinates);
+		for(int i = 0; i < montereyArrayCoords.length; i++)
+		{
+			map.addMarker(new MarkerOptions().position(montereyArrayCoords[i]).title(i+" pos"));
+		}
 	}
 
 	@Override
@@ -46,4 +66,60 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public class MarqueeChangeTask extends AsyncTask {
+		long startTime = 0;
+		long nextTime = 2*(60*1000);
+		Calendar c;
+		@Override
+		protected Object doInBackground(Object... arg0) {
+			if(c.getTimeInMillis()-startTime >= nextTime)
+			{
+				RestartTime(c);
+				//Change text
+			}
+			return null;
+		}
+		
+		protected void onPreExecute()
+		{
+			c = Calendar.getInstance();
+			RestartTime(c);
+		}
+		
+		private void RestartTime(Calendar cal)
+		{
+			startTime = cal.getTimeInMillis();
+		}
+	}
+	
+	LatLng[] GetGpsCoordsFromResource(int res)
+	{
+		try {
+			ArrayList<LatLng> coords = new ArrayList<LatLng>();
+			InputStream in = getResources().openRawResource(res);
+			XMLParser parser = new XMLParser();
+		
+			Document doc = parser.ParseXMLToDoc(in);
+			NodeList nList = parser.ParseDocByTagName(doc, "lat_long_pts");
+			for(int i = 0; i < nList.getLength(); i++)
+			{
+				long lat = 0, lng = 0;
+				Element ele = (Element) nList.item(i);
+				lat = Long.parseLong(parser.GetTextValueByTagName(ele, "lat"));
+				lng = Long.parseLong(parser.GetTextValueByTagName(ele, "lat"));
+				coords.add(new LatLng(lat,lng));
+			}
+			LatLng[] latlngarray = new LatLng[coords.size()];
+			coords.toArray(latlngarray);
+			return latlngarray;
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		return null;
+	} 
+	
 }
